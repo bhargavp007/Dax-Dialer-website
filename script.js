75c2c64057d56f8c1f6c9343a226fd5baf248ce9
bhargavp007 <patelbhargav8262@gmail.com>
2025-07-17 15:02:48 +0530

    Add images and update design sections

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ AOS.init();
 
 document.querySelectorAll('.btn').forEach(btn => {
   btn.addEventListener('click', () => {
-    alert('Thank you for choosing Dax Dialer!');
+    alert('Thanks for choosing DaxDialer!');
   });
 });
